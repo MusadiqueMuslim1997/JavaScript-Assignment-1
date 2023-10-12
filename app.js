e18fba1dc1b5d1c0f80db81965359ff813ec1935
Musadique Muslim <musadique1997@gmail.com>
2023-10-12 22:47:51 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,112 @@
+// Chapter 1
+
+
+// alert("Error! Please enter a valid password")
+
+// alert("Welcome to JS land.... \n Happy Coding!")
+
+// alert("Welcome to JS land....")
+// alert("Happy Coding! \n Prevent this page from creating additional dialogs.")
+
+// var a = "Hello... I can run JS through my web's browser console"
+
+// console.log(a)
+
+
+
+
+
+
+
+// Chapter 2
+
+
+// var userName
+// var myName = "Musadique Muslim"
+// alert(myName);
+
+// var message 
+// message = "Hello World";
+// alert(message);
+
+// var a = "Jhone Doe";
+// var b = "15years";
+// var c = "Certified Mobile App Development";
+// alert(a);
+// alert(b);
+// alert(c);
+
+// var a = "PIZZA \nPIZZ \n PIZ \n PI \n P ";
+// alert(a);
+// var a = "My email adress is";
+// var b = "musadique1997@gmail.com";
+// var c = a +" "+ b;
+// alert(c);
+// var book = "A smarter way to learn JavaScript"
+// var b = "I am trying to learn from the Book" + " " + book ;
+// alert(b);
+
+// var a = "Yah! I can write HTML content through Javascript"
+// document.write(a);
+
+
+// var a = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(a);
+
+
+
+
+
+
+// Chapter 3
+
+// var age = "I am 26 years old";
+// alert(age);
+
+// var a = "You have visited this site 14 times";
+// alert(a);
+
+// var number = "My birth year is 1997";
+// document.write(number);
+
+// var a = "John Doe";
+// var b = "T-Shirt(s)";
+// var c = "5";
+// var d = a + " "  + "Ordered" + " "  + c + b + " " + "on XYZ Clothing store" ;
+// document.write(d);
+
+
+
+// Chapter 4
+
+// var a, b, c;
+
+
+// var userName, myName, fatherName, user_name, your_password 
+
+// var a = "A heading stating “Rules for naming JS variables";
+// document.write(a)
+
+// var a = "numbers"
+// var b = "$"
+// var c = "_"
+// d = "Variable names can only" + a + ","  + b + "and" + c + " "+ "For example $my_1stVariable";
+// document.write(d)
+
+
+
+// var e = "letter"
+// var f ="$"
+// var g = "_"
+// h = "Variables must begin with a" + e + "," + f + " " +"and" + g + "For example $name, _name or name"
+// document.write(h)
+
+
+// var i = "Sensitive"
+// var j = "Variable names are case" + " " + i
+// document.write(j)
+
+
+// var k =  "keywords"
+// var l = "Variable names should not be JS" + " " + k
+// document.write(l)
